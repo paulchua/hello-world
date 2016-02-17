d36aee123d20e58e32e07dc6caf6e639336cdab2
Paul Chua <paulchua@g.harvar.edu>
2016-02-16 22:53:24 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<h1>Hello World!</h1>
+<html>
+<body>
+                <h1>Paul Chua</h1>
+
+                <img src='http://alexachua.com/background.jpg'>
+
+                <h3>PHP Code Generator</h3>
+                <p>View: [link]</p>
+                <p>Github: [link]</p>
+
+                <h3>Project 3 Title</h3>
+                <p>View: [link]</p>
+                <p>Github: [link]</p>
+
+                <h3>Project 4 Title</h3>
+                <p>View: [link]</p>
+                <p>Github: [link]</p>
+
+</body>
+</html>
